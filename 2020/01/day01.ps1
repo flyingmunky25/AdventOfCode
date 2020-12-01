3239aed944b0bd41bec56951dd953f6a98bfe3f5
flyingmunky25 <plevajt@gmail.com>
2020-12-01 17:06:43 -0600

    Adding powershell solution

--- 2020/01/day01.ps1
+++ 2020/01/day01.ps1
@@ -1,13 +1,36 @@
-$input = Get-Content input.txt
+$input = Get-Content input.txt | %{[int]$_}
+
+$foundPartOne = $false
+$foundPartTwo = $false
 
 for ($i = 0; $i -lt $input.Length; $i++)
 {
+    if ( $foundPartOne )
+    {
+        break
+    }
+
     for ($j = 0; $j -lt $input.Length; $j++)
     {
-        $curSum = $input[$i] + $input[$j]
-        if ( $curSum -eq 2020)
+        if ( $input[$i] + $input[$j] -eq 2020)
         {
             Write-Host "$($input[$i] * $input[$j])"
+            $foundPartOne = $true
+        }
+
+        for ($k = 0; $k -lt $input.Length; $k++)
+        {
+            if ( $foundPartTwo )
+            {
+                break                
+            }
+
+            if ( ($input[$i] + $input[$j] + $input[$k]) -eq 2020)
+            {
+                Write-Host "$($($input[$i] * $input[$j] * $input[$k]))"
+                $foundPartTwo = $true
+                break
+            }
         }
     }
 }
