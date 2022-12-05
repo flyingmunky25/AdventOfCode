2f082bb92914dc61c4b76d86d8d44fb44c959d07
Pleva <justin.pleva@tdsinc.com>
2022-12-05 15:24:18 -0600

    pushing day 4 code

--- 2022/04/day04.py
+++ 2022/04/day04.py
@@ -4,4 +4,18 @@ current_dir = dirname(__file__)
 file_path = join(current_dir, "./input.txt")
 with open(file_path, 'r') as f:
     lines = f.readlines()
+    
+numOverlaps = 0
 
+for team in lines:
+    pair = team.strip().split(',')
+    assignments = []
+    xStart = int(pair[0].split('-')[0])
+    xEnd = int(pair[0].split('-')[1])
+    yStart = int(pair[1].split('-')[0])
+    yEnd = int(pair[1].split('-')[1])
+    
+    if ((yStart >= xStart and yEnd <= xEnd) or (xStart >= yStart and xEnd <= yEnd)):
+        numOverlaps += 1
+        
+print(numOverlaps)
