ba5aa5e219a050ecfb560d12d4082f4bd6f2e727
Pleva <justin.pleva@tdsinc.com>
2022-12-05 16:34:23 -0600

    latest changes

--- 2022/04/day04.py
+++ 2022/04/day04.py
@@ -5,17 +5,29 @@ file_path = join(current_dir, "./input.txt")
 with open(file_path, 'r') as f:
     lines = f.readlines()
     
-numOverlaps = 0
+partOneOverlaps = 0
+partTwoOverlaps = 0
+
+allOverlappingValues = []
 
 for team in lines:
     pair = team.strip().split(',')
-    assignments = []
+
     xStart = int(pair[0].split('-')[0])
     xEnd = int(pair[0].split('-')[1])
     yStart = int(pair[1].split('-')[0])
     yEnd = int(pair[1].split('-')[1])
     
+    xValues = range(xStart,xEnd)
+    
+    for y in range(yStart,yEnd):
+        if y in xValues:
+            allOverlappingValues.append(y)
+            
     if ((yStart >= xStart and yEnd <= xEnd) or (xStart >= yStart and xEnd <= yEnd)):
-        numOverlaps += 1
+        partOneOverlaps += 1
         
-print(numOverlaps)
+print("Part One:",partOneOverlaps)
+
+partTwoUnique = [*set(allOverlappingValues)]
+print("Part Two:", len(partTwoUnique))
