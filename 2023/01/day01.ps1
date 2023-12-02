d6daeee2307c6051a08b222c3d445ae9db77a127
Justin Pleva <plevajt@Justins-Air.lan>
2023-12-01 21:37:09 -0600

    finished day 1

--- 2023/01/day01.ps1
+++ 2023/01/day01.ps1
@@ -1,26 +1,38 @@
-$inputFile = Get-Content .\myinput.txt
+$inputFile = Get-Content .\myInput.txt
 $ErrorActionPreference = "Stop"
 $wordToDigitMap = @{
-    "one"   = 1
-    "two"   = 2
-    "three" = 3
-    "four"  = 4
-    "five"  = 5
-    "six"   = 6
-    "seven" = 7
-    "eight" = 8
-    "nine"  = 9
+    "one"   = "o1e"
+    "two"   = "t2o"
+    "three" = "t3e"
+    "four"  = "f4r"
+    "five"  = "f5e"
+    "six"   = "s6x"
+    "seven" = "s7n"
+    "eight" = "e8t"
+    "nine"  = "n9e"
 }
 
-$runningTotal = 0
+$partOneTotal = 0
+$partTwoTotal = 0
 
-foreach ( $line in $inputFile )
-{
-    $digits = Select-String '\d{1}' -InputObject $line -AllMatches
-    
-    $compiledNum = [int]($digits.Matches[0].value + $digits.Matches[$digits.Matches.length - 1].value)
+foreach ( $line in $inputFile ) {
+    # Collect the values to part 1
+    $digitsPartOne = Select-String '\d{1}' -InputObject $line -AllMatches
+    if ( $digitsPartOne.Count -gt 0) {
+        $partOneNum = [int]($digitsPartOne.Matches[0].value + $digitsPartOne.Matches[$digitsPartOne.Matches.Count - 1].value)
+        $partOneTotal += $partOneNum
+    }
 
-    $runningTotal += $compiledNum
+    # Collect the values for part 2
+    $tmpLine = $line
+    foreach ( $value in $wordToDigitMap.GetEnumerator()) {
+        $tmpLine = $tmpLine -replace $value.Key, $value.Value
+    }
+
+    $digitsPartTwo = Select-String '\d{1}' -InputObject $tmpLine -AllMatches
+    $partTwoNum = [int]($digitsPartTwo.Matches[0].value + $digitsPartTwo.Matches[$digitsPartTwo.Matches.Count - 1].value)
+    $partTwoTotal += $partTwoNum
 }
 
-Write-Host "Part One: $runningTotal"
+Write-Host "Part One: $partOneTotal"
+Write-Host "Part Two: $partTwoTotal"
