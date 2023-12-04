5a3124e78c4b37bc52bfce733a4dc14f7c5bb6c8
Justin Pleva <plevajt@gmail.com>
2023-12-04 07:38:36 -0600

    pushing code

--- 2023/03/day03.ps1
+++ 2023/03/day03.ps1
@@ -1,10 +1,36 @@
 $inputFile = Get-Content .\sampleInput.txt
-$symbols = @('!@#$%^&*()_-=+/,<>[]\{}|`~')
-
+$symbols = '[^a-zA-Z0-9\.]'
+$lineLength = $inputFile[0].Length
+$symbolIndexes = @{
+    periods       = @()
+    symbols       = @()
+    indexesToSkip = $symbolIndexes.periods + $symbolIndexes.symbols
+}
 $inputArray = @()
-$2Darray = @()
 
 $inputArray = $inputFile -split '`\r`\n'
+
+$charCount = 0
+
+# Get the indexes of all symbols
+foreach ($line in $inputFile)
+{
+    for ( $i = 0; $i -lt $lineLength; $i++ )
+    {
+        if ( $line[$i] -match $symbols)
+        {
+            $symbolIndexes.symbols += $charCount
+        }
+        elseif ( $line[$i] -eq '.')
+        {
+            $symbolIndexes.periods += $charCount
+        }
+        $charCount++
+    }
+}
+
+# Reset this back to zero and keep count of where we are
+$charCount = 0
 
 for ( $i = 0; $i -lt $inputArray.Count; $i++)
 {
@@ -12,7 +38,32 @@ for ( $i = 0; $i -lt $inputArray.Count; $i++)
     
     for ( $j = 0; $j -le $line.Count; $j++ )
     {
-        # am I a period, symbol
+        # Are you a symbol or a period?
+        if ($charCount -in $symbolIndexes.indexesToSkip)
+        {
+            #just increment the counter and carry on
+            $charCount++
+            continue;
+        }
+        
+        # I was digit
+        # Look and see if there's more digits after me
+        $curDigits = $inputArray[$i][$j]
+        $numSpacesToAdvance = 0
+        for ( $k = $j+1; $k -le $lineLength - $j; $k++)
+        {
+            if ( $inputArray[$i][$k] -match '[0-9]')
+            {
+                $curDigits += $inputArray[$i][$k]
+                $numSpacesToAdvance++
+            }
+            else {
+                break;
+            }
+        }
+
+        $j += $numSpacesToAdvance
+        $charCount += $numSpacesToAdvance + 1
     }
 }
 
